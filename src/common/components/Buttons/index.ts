a7fa3df15f179ec3c3531e581759c997e346adfd
Aleyna Eser <aleynaeser345.ma@gmail.com>
2024-12-07 22:02:36 +0300

    removed unused components

--- src/common/components/Buttons/index.ts
+++
@@ -1,4 +0,0 @@
-import SubmitButton from './SubmitButton';
-import ActionButton from './ActionButton';
-
-export { SubmitButton, ActionButton };
--- src/common/components/SCIcon/sm-icon-set.tsx
+++ src/common/components/SCIcon/sm-icon-set.tsx
@@ -1,29 +1,8 @@
 import { BsExclamation } from 'react-icons/bs';
-import { TbArrowUpRight, TbMoneybag, TbSun, TbMoon, TbEdit } from 'react-icons/tb';
-import {
-  LuAlertCircle,
-  LuChevronLeft,
-  LuChevronRight,
-  LuTrash2,
-  LuX,
-  LuArrowDownToLine,
-  LuCheckCircle2,
-  LuInfo,
-} from 'react-icons/lu';
+import { LuChevronLeft, LuChevronRight } from 'react-icons/lu';
 
 export const SCIconSet = {
-  'sc-money-bg': TbMoneybag,
-  'sc-theme-light': TbSun,
-  'sc-theme-dark': TbMoon,
   'sc-exclamation-mark': BsExclamation,
-  'sc-circle-alert': LuAlertCircle,
-  'sc-circle-check': LuCheckCircle2,
-  'sc-circle-info': LuInfo,
-  'sc-close': LuX,
   'sc-chevron-right': LuChevronRight,
   'sc-chevron-left': LuChevronLeft,
-  'sc-delete': LuTrash2,
-  'sc-edit': TbEdit,
-  'sc-arrow-down': LuArrowDownToLine,
-  'sc-navigate': TbArrowUpRight,
 };
